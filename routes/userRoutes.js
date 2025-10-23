ff0a25ec0d9509b7254418b950587f1abcc702a5
ARDRASATHYAN <sardra9988@gmail.com>
2025-10-23 10:43:13 +0530

    fix(userroutes):middleware added to the userroutes like edit add delete etc

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const { getUsers, getUser, createUser, updateUser, deleteUser, login, refreshToken, requestPasswordReset, resetPassword } = require("../controllers/userController");
+const authenticateToken = require("../middleware/authMiddleware");
 const userrouter = express.Router();
 
 
-userrouter.get("/",getUsers);
-userrouter.get("/:id", getUser);      
-userrouter.post("/", createUser);     
-userrouter.put("/:id", updateUser);    
-userrouter.delete("/:email", deleteUser); 
+userrouter.get("/",authenticateToken,getUsers);
+userrouter.get("/:id",authenticateToken, getUser);      
+userrouter.post("/",authenticateToken, createUser);     
+userrouter.put("/:id",authenticateToken, updateUser);    
+userrouter.delete("/:email",authenticateToken,deleteUser); 
 userrouter.post("/login", login);
 userrouter.post("/refresh-token", refreshToken);
 
