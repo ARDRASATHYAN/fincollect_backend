a592b468ece16b4832f8f411352c364498f947ad
ARDRASATHYAN <sardra9988@gmail.com>
2025-12-03 10:14:25 +0530

    fix(server.js):add middleware to trim data

--- server.js
+++ server.js
@@ -18,6 +18,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+// trim data before sending (middleware)
+app.use((req, res, next) => {
+  if (req.body && typeof req.body === "object") {
+    for (let key in req.body) {
+      if (typeof req.body[key] === "string") {
+        req.body[key] = req.body[key].trim();
+      }
+    }
+  }
+  next();
+});
+
 // require('./models/userModel');
 
 app.use('/banks', bankrouter);
